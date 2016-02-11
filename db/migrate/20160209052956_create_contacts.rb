9489ca2ea3ee6e17eff7a8f5893311877844c9fe
Josh Ludvigson <joshludvigson@gmail.com>
2016-02-11 04:10:24 +0000

    Created contact form view

--- db/migrate/20160209052956_create_contacts.rb
+++ db/migrate/20160209052956_create_contacts.rb
@@ -3,7 +3,6 @@ class CreateContacts < ActiveRecord::Migration
     create_table :contacts do |t|
       t.string :name
       t.string :email
-      t.string :phone
       t.text :comments
       t.timestamps
     end
