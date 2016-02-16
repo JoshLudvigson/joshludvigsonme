2780151ae5a7db9939dad57ab3eec75f7c856ed8
Josh Ludvigson <joshludvigson@gmail.com>
2016-02-16 06:10:49 +0000

    renamed bg image

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/software' => 'pages#software'
   get '/projects' => 'pages#projects'
   root 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
